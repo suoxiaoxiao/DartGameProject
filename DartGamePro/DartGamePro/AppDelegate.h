618ff4000ec427c6681da26d0ceb7ff1ba8fa88b
suoxiaoxiao <suoxiaoxiao>
2016-08-20 17:11:58 +0800

    first Commit

---
+++ DartGamePro/DartGamePro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DartGamePro
+//
+//  Created by 索晓晓 on 16/8/20.
+//  Copyright © 2016年 SXiao.RR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DartGamePro/DartGamePro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DartGamePro
+//
+//  Created by 索晓晓 on 16/8/20.
+//  Copyright © 2016年 SXiao.RR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
